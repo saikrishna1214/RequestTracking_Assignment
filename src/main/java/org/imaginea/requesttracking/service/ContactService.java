7c9d2744d42da98e7d3fbb07c474832104c325bf
saikrishna1214 <saikrishnadec14@gmail.com>
2018-05-21 17:03:50 +0530

    added comments

--- src/main/java/org/imaginea/requesttracking/service/ContactService.java
+++ src/main/java/org/imaginea/requesttracking/service/ContactService.java
@@ -1,6 +1,20 @@
 package org.imaginea.requesttracking.service;
 
+/**
+ * provides services of contact entity and
+ * maps it to a row in Contact table in 
+ * database
+ * @author saikrishnak
+ *
+ */
+
 public interface ContactService {
+	
+	/**
+	 * creates contact and maps it to a entity in
+	 * table in database. it calls a Sessionutil method 
+	 * which opens a session for it 
+	 */
 	
 	public void createContact();
 
