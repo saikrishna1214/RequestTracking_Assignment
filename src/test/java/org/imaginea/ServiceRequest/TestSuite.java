51cd575a3c60693a48113cae6fe89e580af58a1a
saikrishna1214 <saikrishnadec14@gmail.com>
2018-05-22 09:09:36 +0530

    added testsuite class

---
+++ src/test/java/org/imaginea/ServiceRequest/TestSuite.java
@@ -0,0 +1,15 @@
+package org.imaginea.ServiceRequest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+
+@SuiteClasses({AppTest.class})
+
+public class TestSuite {
+	
+
+}
